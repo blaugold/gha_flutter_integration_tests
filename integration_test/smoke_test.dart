305760843c947e91be46e51c044bdd7b22f09c7b
Gabriel Terwesten <gabriel@terwesten.net>
2024-09-16 20:00:36 +0200

    Abort test

--- integration_test/smoke_test.dart
+++ integration_test/smoke_test.dart
@@ -1,5 +1,14 @@
+import 'dart:ffi';
+
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('smoke', (tester) async {});
+  testWidgets('smoke 1', (tester) async {});
+  testWidgets('smoke 2', (tester) async {
+    await Future.delayed(const Duration(seconds: 10));
+    abort();
+  });
 }
+
+final process = DynamicLibrary.process();
+final abort = process.lookupFunction<Void Function(), void Function()>("abort");
